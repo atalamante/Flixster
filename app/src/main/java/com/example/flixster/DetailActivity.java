8d1d82fb93da0f5ae1cd9fc1f17aca47e86126ab
atalamante <atalamante625@gmail.com>
2021-02-21 00:20:00 -0700

    Details Screen Implementation

---
+++ app/src/main/java/com/example/flixster/DetailActivity.java
@@ -0,0 +1,33 @@
+package com.example.flixster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.example.flixster.models.Movie;
+
+import org.parceler.Parcels;
+
+public class DetailActivity extends AppCompatActivity {
+
+    TextView tvTitle;
+    TextView tvOverview;
+    RatingBar ratingBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        tvTitle = findViewById(R.id.titleTv);
+        tvOverview = findViewById(R.id.overviewTV);
+        ratingBar = findViewById(R.id.ratingBar);
+
+        Movie movie = Parcels.unwrap(getIntent().getParcelableExtra("movie"));
+        tvTitle.setText(movie.getTitle());
+        tvOverview.setText(movie.getOverview());
+        ratingBar.setRating((float) movie.getRating());
+    }
+}
